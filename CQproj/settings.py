a8db5c71fbccc1ae5e7a9bfcaade418f1a409273
sai7626 <sainathpacharne7777@gmail.com>
2022-08-13 22:43:25 +0530

    Project added

--- CQproj/settings.py
+++ CQproj/settings.py
@@ -124,7 +124,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
